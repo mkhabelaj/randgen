ff82068a6802c960381c4dda2d34063a9e9e58ac
jackson <jackson@swordfish.co.za>
2018-01-24 06:18:52 +0200

    generate random string

--- stringgen/generator.py
+++ stringgen/generator.py
@@ -1,13 +1,15 @@
+import random
 import socket
+import string
 
 
 class Generator:
 
-    def __init__(self,string_size=20, number_of_strings=1):
-        self.string_size = string_size
+    def __init__(self, string_length=20):
+        self.string_length = string_length
 
     def produce_strings(self):
-        result = []
+        result = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(self.string_length))
 
 
 
